1aefa11bfa359aaa6715d540ccd115d8d8151559
Mikelabu <michael.labu@gmail.com>
2021-08-14 13:50:02 +1200

    Added website files

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+const path = require('path');
+
+
+
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + ''));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
