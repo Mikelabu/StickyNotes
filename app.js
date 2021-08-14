f7024febbf0195c553db8cddce2a34e14c4fff1b
chujoel <65984835+chujoel@users.noreply.github.com>
2021-08-15 09:43:35 +1200

    Added button for Mike to work on

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var express = require('express'); //these are the packages we need to be using
-const path = require('path'); //another package
+var express = require('express');
+const path = require('path');
 
 
 
 
 var app = express();
-var port = process.env.PORT || 3000; //use the specified port (variable that heroku looks for), or 3000
+var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '')); //this is where the static (Frontend) files eg css (dirname is wherever it is right now)
+app.use(express.static(__dirname + ''));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html')); //this is where to look for the html file
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port, function () {
-    console.log('Example app listening on port ' + port + '!'); //first line of code run after root is set up
+    console.log('Example app listening on port ' + port + '!');
 });
