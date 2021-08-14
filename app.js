e8927aa4b807920d8ca19b674c502cef78eb26ee
pinkfrangipani <50606695+pinkfrangipani@users.noreply.github.com>
2021-08-15 10:34:17 +1200

    Update app.js
    re-added comments

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const xlsxFile = require('read-excel-file/node') // package used for reading exc
 
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000; //use the specified port (variable that heroku looks for), or 3000
 
-app.use(express.static(__dirname + ''));
+app.use(express.static(__dirname + '')); //this is where the static (Frontend) files eg css (dirname is wherever it is right now)
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html')); //this is where to look for the html file
 });
 
 app.listen(port, function () {
-    console.log('Example app listening on port ' + port + '!');
+    console.log('Example app listening on port ' + port + '!'); //first line of code run after root is set up
 });
 
 var data; // 2D array with excel file data
