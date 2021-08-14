984ca183b6424ebc809054208d426001a0c88e06
deswa7310 <johnhfflynn25@gmail.com>
2021-08-15 10:12:47 +1200

    Added scenario dictionary

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-var express = require('express');
-const path = require('path');
-
-
+var express = require('express'); //these are the packages we need to be using
+const path = require('path'); //another package
+const xlsxFile = require('read-excel-file/node') // package used for reading excel files
 
 
 var app = express();
@@ -16,3 +15,26 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
     console.log('Example app listening on port ' + port + '!');
 });
+
+var data; // 2D array with excel file data
+var scenarios; // Dictionary (map) of different scenarios and options:
+
+// Converts excel file into 2D array:
+xlsxFile('./household-expenditure-statistics-year-ended-june-2019.xlsx').then((rows) => {
+    data = rows;
+    scenarios =
+    {
+        transport : {
+            description : "You are going to school. What option do you choose?",
+            stat : "The average household weekly expenditure on transport is $20.50",
+            options : [
+                {
+                    description : "Take the train."
+                },
+                {
+                    description : "Take the car."
+                }
+            ]
+        }
+    };
+})
